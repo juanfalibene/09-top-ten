c50822d6c21c561a9d6df7dccb0b9e1098dda4c6
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-05-01 19:18:36 +0200

    list created

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const draggableList = document.getElementById("draggable-list");
-const checkButton = document.getElementById("chek");
+console.group(draggableList);
+const checkButton = document.getElementById("check");
 
 const healtyFood = [
   "Water",
@@ -13,3 +14,30 @@ const healtyFood = [
   "Flaxseed, nuts and seeds",
   "Organic yogurt",
 ];
+
+// guardar list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// insertar list items en el DOM
+function createList() {
+  // spread oprator para genera copia
+  [...healtyFood].forEach((food, index) => {
+    const listItem = document.createElement("li");
+    // creaer custom attribute para difenciarlo
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `<span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+    <p class="food-name">${food}</p>
+    <span class="icon la--grip-lines-vertical"></span>
+    </div>`;
+
+    listItems.push(listItem);
+
+    draggableList.appendChild(listItem);
+  });
+}
