589475bea759be1a5ef2d02e54e742cb8ef5de31
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-05-01 22:35:18 +0200

    functions and events created

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const draggableList = document.getElementById("draggable-list");
-console.group(draggableList);
 const checkButton = document.getElementById("check");
 
 const healtyFood = [
@@ -36,16 +35,91 @@ function createList() {
     .forEach((food, index) => {
       const listItem = document.createElement("li");
       // creaer custom attribute para difenciarlo
+
       listItem.setAttribute("data-index", index);
 
       listItem.innerHTML = `<span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
     <p class="food-name">${food}</p>
-    <span class="icon la--grip-lines-vertical"></span>
     </div>`;
 
       listItems.push(listItem);
 
       draggableList.appendChild(listItem);
     });
+
+  addEventListener();
 }
+
+function dragStart() {
+  //console.log("Event ", "dragstart");
+  // tomar el atributo custom (index) de elemento mas cercano
+  // + delante de this para convertirlo en numero
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+
+function dragEnter() {
+  //console.log("Event ", "dragenter");
+  //usar this que pertecene a la lista de los elementos
+  this.classList.add("over");
+}
+function dragLeave() {
+  //console.log("Event ", "dragsleave");
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  //console.log("Event ", "dragover");
+  e.preventDefault();
+}
+function dragDrop() {
+  //console.log("Event ", "drop");
+  const dragEndIndex = +this.getAttribute("data-index");
+  // controla el final
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+// switchear los div
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+// check orden de list items .trim() para evitar espacion en blanco en el nombre
+function checkOrder() {
+  console.log("check");
+  listItems.forEach((listItem, index) => {
+    const foodName = listItem.querySelector(".draggable").innerText.trim();
+    console.log(foodName, index);
+
+    // match index de items ordenados segun usuario y array
+    if (foodName !== healtyFood[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+}
+
+function addEventListener() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+checkButton.addEventListener("click", checkOrder);
