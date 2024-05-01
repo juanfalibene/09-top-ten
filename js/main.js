de70893128c6a4935c2f11e82bbc2625a2be5847
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-05-01 19:29:33 +0200

    random order list

--- js/main.js
+++ js/main.js
@@ -24,20 +24,28 @@ createList();
 
 // insertar list items en el DOM
 function createList() {
-  // spread oprator para genera copia
-  [...healtyFood].forEach((food, index) => {
-    const listItem = document.createElement("li");
-    // creaer custom attribute para difenciarlo
-    listItem.setAttribute("data-index", index);
+  // spread operator para genera copia
+  // cambiar array a array de objetos
+  // cambiar el orden con un value(decimal) random
+  // mapear el value(nombre de food)
+  // recorrer y crear li
+  [...healtyFood]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((food, index) => {
+      const listItem = document.createElement("li");
+      // creaer custom attribute para difenciarlo
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `<span class="number">${index + 1}</span>
+      listItem.innerHTML = `<span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
     <p class="food-name">${food}</p>
     <span class="icon la--grip-lines-vertical"></span>
     </div>`;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggableList.appendChild(listItem);
-  });
+      draggableList.appendChild(listItem);
+    });
 }
