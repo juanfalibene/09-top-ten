ecd962576969cccca33f8bc58c08c8dddfc36732
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-05-01 18:20:56 +0200

    project init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const draggableList = document.getElementById("draggable-list");
+const checkButton = document.getElementById("chek");
+
+const healtyFood = [
+  "Water",
+  "Dark green vegetables",
+  "Whole grains",
+  "Beans and lentils",
+  "Fish",
+  "Berries",
+  "Winter squash",
+  "Soy",
+  "Flaxseed, nuts and seeds",
+  "Organic yogurt",
+];
